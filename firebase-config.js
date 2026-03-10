9bc81aa765c0abf862474ca3bf07de9253ee373f
alishanpanel0002-lang <alishanpanel0002@gmail.com>
2026-03-10 21:03:35 +0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -10,12 +10,12 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase
 // ⬇ Replace these with your Firebase project values
 // Firebase Console → Project Settings → General → Your Apps → Web
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCzYZihIYa2S3OJTvkpHWJqOrFhJYzPMqM",
+  authDomain: "authforge-5d068.firebaseapp.com",
+  projectId: "authforge-5d068",
+  storageBucket: "authforge-5d068.firebasestorage.app",
+  messagingSenderId: "653397180076",
+  appId: "1:653397180076:web:99f8f48882b4422504a909"
 };
 
 const app = initializeApp(firebaseConfig);
